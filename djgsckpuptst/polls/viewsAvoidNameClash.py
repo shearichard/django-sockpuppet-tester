05ba5f04807919d868fb1a24bca1b09d6d686806
Richard Shea <rshea@thecubagroup.com>
2021-03-09 11:07:53 +1300

    Working list of todos

--- djgsckpuptst/polls/viewsAvoidNameClash.py
+++ djgsckpuptst/polls/viewsAvoidNameClash.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+
+from .models import Todo
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    todo_list = Todo.objects.order_by('title')
+    import pprint
+    pprint.pprint(todo_list)
+    template = loader.get_template('todo/index.html')
+    context = {
+        'todo_list': todo_list,
+    }
+    return HttpResponse(template.render(context, request))
